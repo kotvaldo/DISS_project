6f08d21aa3425e843d1f3f846bf09da1ac76df49
Adam Staňo <adamstano5@gmail.com>
2025-03-07 08:32:23 +0100

    commit

--- src/main/java/Generators/BaseGenerator.java
+++ src/main/java/Generators/BaseGenerator.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public abstract class BaseGenerator<T extends Number> {
     protected Random baseRandom;
+    protected SeedGenerator seedGenerator;
     protected int seed;
 
     protected BaseGenerator(int seed) {
         this.seed = seed;
         baseRandom = new Random(seed);
+        seedGenerator = new SeedGenerator();
     }
 
     protected BaseGenerator() {
