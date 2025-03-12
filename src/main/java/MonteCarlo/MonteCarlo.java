cfcfbee705f25a55e1b8f6ef9adaba9f2332a691
Adam Staňo <adamstano5@gmail.com>
2025-03-12 16:06:10 +0100

    adapted to 70 % of reps

--- src/main/java/MonteCarlo/MonteCarlo.java
+++ src/main/java/MonteCarlo/MonteCarlo.java
@@ -45,7 +45,7 @@ public class MonteCarlo extends SimulationCore {
             params.setHeadlightsDemand(headlightsDemand);
             params.setSuppressorsDemand(suppressorsDemand);
             params.setBreakPlatesDemand(breakPlatesDemand);
-            params.setTargetRepCount((int) (this.repCount * 0.1));
+            params.setTargetRepCount((int) (this.repCount * 0.7));
             params.setDailyFineCosts(dailyFineCosts);
             params.setTargetAVGRepCount((int) (this.repCount > 1000 ? 1000 : 0.5 * this.repCount));
             totalCost = strategy.algorithm(params);
@@ -72,7 +72,7 @@ public class MonteCarlo extends SimulationCore {
         this.actualRepCount++;
         dailyCostsCopy = new ArrayList<>(dailyCosts);
         dailyFineCostsCopy = new ArrayList<>(dailyFineCosts);
-        if(actualRepCount < (int) (this.repCount * 0.1)) {
+        if(actualRepCount < (int) (this.repCount * 0.7)) {
             this.dailyCosts.clear();
             this.dailyFineCosts.clear();
         }
